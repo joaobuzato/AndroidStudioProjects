f5640b4a2a1f6e841b48fc005a6ac2c45aeb5d9a
Buzato <joaobuzato@gmail.com>
2022-01-23 15:50:12 -0300

    segundo commit android

--- Aluno/app/src/main/java/com/example/aluno/MainActivity.java
+++ Aluno/app/src/main/java/com/example/aluno/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.aluno;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "Olá Mundo!", Toast.LENGTH_LONG).show();
+    }
 }
